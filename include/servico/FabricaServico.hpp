77fa43c8e285ab741e9e1f35552000d6e486f3ad
Tarsila Marques <tarsilamarques07@gmail.com>
2025-11-20 17:05:06 -0300

    add os outros serviços na fabricaservico

--- include/servico/FabricaServico.hpp
+++ include/servico/FabricaServico.hpp
@@ -16,11 +16,42 @@ class FabricaServico {
 public:
     virtual ~FabricaServico() {}
 
+    // --- MÉTODOS EXISTENTES (Autenticação) ---
     /**
      * @brief Cria e retorna uma instância do Serviço de Autenticação.
-     * @return Ponteiro para a interface ILNAutenticacao.
+     * @return Ponteiro para a interface IServicoAutenticacao.
      */
     virtual IServicoAutenticacao* criarServicoAutenticacao() = 0;
+    
+    /**
+     * @brief Cria e retorna uma instância do Serviço de Gerenciamento de Gerentes (CRUD).
+     * @return Ponteiro para a interface IServiçoGerente.
+     */
+    virtual IServicoGerente* criarServicoGerente() = 0;
+    
+    /**
+     * @brief Cria e retorna uma instância do Serviço de Gerenciamento de Hóspedes.
+     * @return Ponteiro para a interface IServiçoHospede.
+     */
+    virtual IServicoHospede* criarServicoHospede() = 0;
+
+    /**
+     * @brief Cria e retorna uma instância do Serviço de Hotéis.
+     * @return Ponteiro para a interface IServiçoHotel.
+     */
+    virtual IServicoHotel* criarServicoHotel() = 0;
+
+    /**
+     * @brief Cria e retorna uma instância do Serviço de Quartos.
+     * @return Ponteiro para a interface IServiçoQuarto.
+     */
+    virtual IServicoQuarto* criarServicoQuarto() = 0;
+
+    /**
+     * @brief Cria e retorna uma instância do Serviço de Reservas.
+     * @return Ponteiro para a interface IServiçoReservas.
+     */
+    virtual IServicoReservas* criarServicoReservas() = 0;
 };
 
 #endif // FABRICASERVICO_HPP
